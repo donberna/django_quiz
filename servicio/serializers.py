b1a85036b231df98c65a98b4c4e4a04143c5af25
DonB <donberna-93@hotmail.com>
2015-08-10 15:48:30 -0500

    id quiz serializer

--- servicio/serializers.py
+++ servicio/serializers.py
@@ -96,6 +96,7 @@ class Quiz_Serializer(serializers.ModelSerializer):
     class Meta():
         model = Quiz
         fields = ('id' ,'title', 'description', 'url', 'category', 'random_order', 'max_questions', 'answers_at_end', 'exam_paper', 'single_attempt', 'pass_mark', 'success_text', 'fail_text', 'draft', 'get_max_score', 'quiz')
+        read_only_fields = ('id')
 
 
 
