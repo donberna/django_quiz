d906417ea31c70d599c0718c33a608b6daacc604
DonB <donberna-93@hotmail.com>
2015-09-10 11:37:46 -0500

    varios

--- servicio/views.py
+++ servicio/views.py
@@ -90,7 +90,7 @@ class Multichoice_Answer_Create_multiple(APIView):
         #print id_q
         #question = MCQuestion.objects.get(id=id_q)
 
-        answers =  request.POST.list()
+        answers =  request.POST.lists()
 
         print answers
         #print answers['contenido[]']
