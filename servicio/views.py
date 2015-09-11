8885b985aa599197794cd1e61b9594fdc8938743
DonB <donberna-93@hotmail.com>
2015-09-11 15:41:14 -0500

    changes

--- servicio/views.py
+++ servicio/views.py
@@ -391,8 +391,8 @@ class Quiz_Create_Sitting_View(APIView):
 
         # se obtienen el quiz y el usuario 
         quiz = get_object_or_404(Quiz, id=self.kwargs['pk_quiz'])
-        #logged_in_user = request.POST['id']  | User.objects.get(pk=pk)
-        logged_in_user = request.user
+        #logged_in_user = request.POST['id']  | 
+        logged_in_user = User.objects.get(pk= request.POST['id'])
 
         # se llama a la funcion del modelo donde 
         # 1. busca el sitting por usuario y quiz 
